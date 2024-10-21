11e1a99bdb389b903a00c6555c4ca42238408a5a
zeshan <138660474+zeshantech@users.noreply.github.com>
2024-10-21 12:03:01 +0500

    feat: changed hostname

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -6,7 +6,7 @@ console.log(apiUrl, "to run actions");
 
 
 export const API = axios.create({
-  baseURL: apiUrl ?? 'http://35.156.73.243/api',
+  baseURL: apiUrl ?? 'http://3.123.206.246/api',
   headers: {
     "Content-Type": "application/json",
   },
